16c0719de98bc23165d2afae6fa94384e5f27a61
dimi11433 <dimitricromarty@gmail.com>
2025-05-12 16:09:42 -0400

    Finished the script now to automate

---
+++ BashDaily/Day1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILE_PATH="$HOME/Documents/Code/Bash/100DaysofBash/BashDaily/"
+COUNT_PATH="$FILE_PATH/Counter.txt"
+
+if [ ! -f "$COUNT_PATH" ];then
+ touch "$COUNT_PATH"
+fi 
+
+CURRENT=$(cat "$COUNT_PATH")
+NEXT=$((CURRENT + 1))
+
+if [ NEXT -lt 100 ]; then
+ echo "$NEXT" > "$COUNT_PATH"
+ mail -s "Day $NEXT of the challenge" dimitricromarty@gmail.com < "$COUNT_PATH"
+else
+ exit 0
+fi
+

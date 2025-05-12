419b2cca6c2669d8b99b2dc364a8e8ae3d2bd0ff
dimi11433 <dimitricromarty@gmail.com>
2025-05-12 18:08:04 -0400

    Working automation

--- BashDaily/Day1.sh
+++ BashDaily/Day1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
 FILE_PATH="$HOME/Documents/Code/Bash/100DaysofBash/BashDaily/"
 COUNT_PATH="$FILE_PATH/Counter.txt"
 
@@ -12,7 +13,7 @@ NEXT=$((CURRENT + 1))
 
 if [ "$NEXT" -lt 100 ]; then
  echo "$NEXT" > "$COUNT_PATH"
- msmtp dimitricromarty@gmail.com < "$COUNT_PATH"
+ /opt/homebrew/bin/msmtp  dimitricromarty@gmail.com < "$COUNT_PATH"
 else
  exit 0
 fi
